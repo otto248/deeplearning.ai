ec604e6c836dfacd5ec642286a23dabb3ad60f1c
otto248 <yuemengcui@hotmail.com>
2018-06-11 16:32:33 +0800

    KG serach added

--- chatbot_test/chatbot_predict/src/classifier/KG_search/views.py
+++ chatbot_test/chatbot_predict/src/classifier/KG_search/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from . import search
+raw_info = search.read_database()
+
+def search(request):
+    text = request.get('text')
+    matchEntity = search.fuzzymatch(text,raw_info[2])
