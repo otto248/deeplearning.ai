95f352bc3e374ab445fe1386c97e5a363f1885bb
otto248 <yuemengcui@hotmail.com>
2018-06-12 10:16:32 +0800

    add all

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+# TODO: Set to URL of git repo.
+PROJECT_GIT_URL='http://git.yonyou.com/cuiymf/chatbot_test.git'
+
+PROJECT_BASE_PATH='/usr/local/workspace'
+
+
+# Install Python
+echo "Installing dependencies..."
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
+tar xvJf Python-3.6.4.tar.xz
+cd Python-3.6.4
+./configure
+make
+make install
+
+# Install virtualenv
+sudo apt-get install python-pip
+sudo pip install virtualenv
+mkdir ~/.virtualenvs
+sudo pip install virtualenvwrapper
+export WORKON_HOME=$HOME~/.virtualenvs
+sudo source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv chat_bot
+
+# Git clone the project and install dependencies
+sudo mkdir -p $PROJECT_BASE_PATH
+sudo git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/chat_bot
+sudo pip install -r $PROJECT_BASE_PATH/chat_bot/chat_bot_test/requirements.txt
